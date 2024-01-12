c01a8f62da79c180eaa8d71bf1a5892b7426e38a
MEME-KING16 <115302822+MEME-KING16@users.noreply.github.com>
2024-01-11 20:39:35 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+fetch('file:///C:/Users/sebfi/Downloads/my%20gpt/trained-model.json')
+  .then(response => response.json())
+  .then(trainedModel => {
+    const network = new brain.recurrent.LSTM();
+    network.fromJSON(trainedModel);
+
+    // Now you can use the network for predictions
+    const output = network.run('Some input');
+    console.log(output);
+  });
+
+
+function send() {
+  let msg = document.getElementById("text")
+  let chat = document.getElementById("chat")
+// Test the network
+const output = network.run(msg.value.toLowerCase());
+chat.innerHTML += `You: ${msg.value}<br><br>Chat: ${output}<br>`
+msg.value = ""
+}
